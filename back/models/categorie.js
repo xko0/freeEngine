9dfc18eddcd4ccf7b083973248ed89cabad0cdb9
xko0 <germain.rgb@hotmail.fr>
2023-01-18 14:59:32 +0100

    cleaned from old files no longer needed

--- back/models/categorie.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const categorie = mongoose.Schema({
-    nom: {
-      type: String,
-      required: false
-    }
-});
-module.exports = mongoose.model("Categorie", categorie);
