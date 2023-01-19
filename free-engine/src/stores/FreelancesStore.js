9768068b11c28191c4002a12ad2b4fd2f0a4b245
xko0 <germain.rgb@hotmail.fr>
2023-01-19 16:56:02 +0100

    freelancestore restored to origin

--- free-engine/src/stores/FreelancesStore.js
+++ free-engine/src/stores/FreelancesStore.js
@@ -1,4 +1,3 @@
-
 import { runInAction } from 'mobx'
 import axios from 'axios'
 
@@ -18,7 +17,7 @@ export function createFreelancesStore() {
         this.hasErrors = false
       })
       try {
-        let response = await axios.get(`vercel/scrapeMaltData`,{
+        let response = await axios.get(`${BASE_URL}/scrapeMaltData`,{
           params: {
               argument: infos
           }
@@ -67,7 +66,7 @@ export function createFreelancesStore() {
         this.hasErrors = false
       })
       try {
-        let response = await axios.get(`vercel/scrapeFreelanceComData`,{
+        let response = await axios.get(`${BASE_URL}/scrapeFreelanceComData`,{
           params: {
               argument: infos
           }
@@ -87,94 +86,3 @@ export function createFreelancesStore() {
     }
   }
 }
-
-//_____________________________________________________________________________________
-
-// import { runInAction } from 'mobx'
-// import axios from 'axios'
-
-// const BASE_URL = import.meta.env.VITE_API_URL;
-
-// export function createFreelancesStore() {
-//   return {
-//     loading: null,
-//     hasErrors: null,
-//     freelancesMalt: JSON.parse(localStorage.getItem('freelancesMalt')) || [],
-//     freelancesFiverr: JSON.parse(localStorage.getItem('freelancesFiverr')) || [],
-//     freelanceCom: JSON.parse(localStorage.getItem('freelanceCom')) || [],
-
-//     async getFreelances(infos) {
-//       runInAction(() => {
-//         this.loading = true
-//         this.hasErrors = false
-//       })
-//       try {
-//         let response = await axios.get(`${BASE_URL}/scrapeMaltData`,{
-//           params: {
-//               argument: infos
-//           }
-//         }) 
-//         if (response.data) {
-//           runInAction(() => {
-//             this.loading = false
-//             console.log("in progress...")
-//             console.log(response.data)
-//             this.freelancesMalt = response.data
-//             localStorage.setItem('freelancesMalt', JSON.stringify(response.data))
-//           })
-//         }    
-//       } catch(error) {
-//         console.error(error)
-//       }
-//     },
-
-//     async getFiverrFreelances(infos) {
-//       runInAction(() => {
-//         this.loading = true
-//         this.hasErrors = false
-//       })
-//       try {
-//         let response = await axios.get(`${BASE_URL}/scrapeFiverrData`,{
-//           params: {
-//               argument: infos
-//           }
-//         }) 
-//         if (response.data) {
-//           runInAction(() => {
-//             this.loading = false
-//             console.log("in progress...")
-//             console.log(response.data)
-//             this.freelancesFiverr = response.data
-//           })
-//         }    
-//       } catch(error) {
-//         console.error(error)
-//       }
-//     },
-
-//     async getFreelanceCom(infos) {
-//       runInAction(() => {
-//         this.loading = true
-//         this.hasErrors = false
-//       })
-//       try {
-//         let response = await axios.get(`${BASE_URL}/scrapeFreelanceComData`,{
-//           params: {
-//               argument: infos
-//           }
-//         }) 
-//         if (response.data) {
-//           runInAction(() => {
-//             this.loading = false
-//             console.log("Calcul...")
-//             console.log(response.data)
-//             this.freelanceCom = response.data
-//             localStorage.setItem('freelanceCom', JSON.stringify(response.data))
-//           })
-//         }    
-//       } catch(error) {
-//         console.error(error)
-//       }
-//     }
-//   }
-// }
